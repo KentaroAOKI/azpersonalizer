cad3c8a77ba6ce1d301cbc6eb5cd6e3074b05c08
Kentaro Aoki <kentaro.aoki@gmail.com>
2019-05-19 14:15:37 +0900

    update for pypi

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, 'READMEFORPYPI.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='azpersonalizer',
-    version='0.0.1',
+    version='0.0.3',
     description='It is a helper tool for azure cognitive personalizer. It is not official.',
     long_description=long_description,
     long_description_content_type='text/markdown',
